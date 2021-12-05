a262d5fd7fc1e5f1668fbc27f3bcf5ae4787eb0b
Samuel K <mrsosaki@posteo.net>
2021-12-05 15:12:54 +0100

    str2float: remove unnecessary comments

--- src/str2float.py
+++ src/str2float.py
@@ -9,22 +9,11 @@ def str2float(text: str) -> float:
 
     allowed_chars = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.' ]
 
-    # for i in text:
-
-    #     if i == ",":
-    #         if comma_count >= 1:
-    #             text = text.replace(i, ".", 1)
-    #             print(type(print(text)))
-    #             comma_count += 1
-
-    #     if i not in allowed_chars:
-    #         text = text.replace(i, "")
-
-    # return float(text)
     for i in text:
         if i not in allowed_chars:
             text = text.replace(i, "")
     
     return float(text)
+
 if __name__ == "__main__":
     print(str2float("21,123.23"))
